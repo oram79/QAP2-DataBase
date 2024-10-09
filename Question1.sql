bcb13e6c4ad133cd2c40894dbca87b83745ced09
Logan Oram <logan.oram@keyin.com>
2024-10-09 15:19:41 -0230

    Code Update

--- Question1.sql
+++ Question1.sql
@@ -1,3 +1,8 @@
+-- Logan Oram
+-- Oct 9th, 2024
+
+-- Creating Tables
+
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(50),
@@ -29,3 +34,36 @@ CREATE TABLE enrollments (
     FOREIGN KEY (student_id) REFERENCES students(id),
     FOREIGN KEY (course_id) REFERENCES courses(id)
 );
+
+-- INSERTING DATA
+-- Student Data
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('Cole', 'Caufield', 'colecaufield22@gmail.com', '2024-10-09'),
+('Nick', 'Suzuki', 'nicksuzuki14@gmail.com', '2024-10-09'),
+('Juraj', 'Slafkovsky', 'jurajslaf20@gmail.com', '2024-10-09'),
+('Lane', 'Hutson', 'lanehutson48@gmail.com', '2024-10-09'),
+('Kaiden', 'Guhle', 'kaidenguhle21@gmail.com', '2024-10-09');
+
+-- Professor Data
+
+INSERT INTO professors (first_name, last_name, department) VALUES
+('Martin', 'St.Louis', 'Intro to head coaching'),
+('Kent', 'Hughes', 'Intro to Managing'),
+('Geoff', 'Molson', 'Entrepreneurship 1001'),
+('Jeff', 'Gorton', 'Entrepreneurship 2200');
+
+-- Courses Data
+
+INSERT INTO courses (course_name, course_description, professor_id) VALUES
+('Offence', 'Intro To Offence', 2),
+('Defence', 'Intro To Defence', 3),
+('PowerPlay', 'Techniques On Power-Play', 1);
+
+-- Enrollments Data
+INSERT INTO enrollments (student_id, course_id, enrollment_date) VALUES
+(1, 1, '2024-10-09'),
+(2, 1, '2024-10-09'),
+(3, 2, '2024-10-09'),
+(4, 3, '2024-10-09'),
+(5, 1, '2024-10-09');
