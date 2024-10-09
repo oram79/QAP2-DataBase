08ff2bcf09157205227141b05d5766719dd5ed73
Logan Oram <logan.oram@keyin.com>
2024-10-09 19:12:27 -0230

    Update on code

--- Question2.sql
+++ Question2.sql
@@ -1,6 +1,71 @@
+-- Logan Oram
+-- Oct 9th 2024
+
+-- Creating Tables
 CREATE TABLE products (
 id SERIAL PRIMARY KEY,
 product_name VARCHAR(100),
 price DECIMAL(10,2),
 stock_quantity INT
 );
+
+CREATE TABLE customers (
+id SERIAL PRIMARY KEY,
+first_name VARCHAR(100),
+last_name VARCHAR(100),
+email VARCHAR(100)
+);
+
+CREATE TABLE orders (
+id SERIAL PRIMARY KEY,
+customer_id INT,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+CREATE TABLE order_items (
+order_id INT,
+product_id INT,
+quantity INT,
+PRIMARY KEY (order_id, product_id),
+FOREIGN KEY (order_id) REFERENCES orders(id),
+FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+-- INSERTING DATA
+-- Products Data
+
+INSERT INTO products (product_name, price, stock_quantity) VALUES
+('Hoodie', 59.99, 38),
+('Crewneck' 49.99, 26),
+('T-Shirt' 24.99, 21),
+('Jeans' 64.99, 112),
+('Hat' 39.99, 45);
+
+-- Insert Customers
+INSERT INTO customers (first_name, last_name, email) VALUES
+('Alice', 'Williams', 'alice.williams@example.com'),
+('Bob', 'Miller', 'bob.miller@example.com'),
+('Charlie', 'Garcia', 'charlie.garcia@example.com'),
+('David', 'Martinez', 'david.martinez@example.com');
+
+-- Insert Orders
+INSERT INTO orders (customer_id, order_date) VALUES
+(1, '2024-10-01'),
+(2, '2024-10-02'),
+(3, '2024-10-02'),
+(4, '2024-10-04'),
+(3, '2024-10-06');
+
+-- Insert Order Items
+INSERT INTO order_items (order_id, product_id, quantity) VALUES
+(1, 1, 1),
+(1, 2, 2),
+(2, 3, 1),
+(2, 4, 1),
+(3, 5, 1),
+(3, 1, 1),
+(4, 2, 2),
+(4, 3, 1),
+(5, 4, 3),
+(5, 5, 1);
